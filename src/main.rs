9c8049544bb61954d1ce8cc644526ca0c70dedc6
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-21 21:58:49 -0500

    refactor: created echo endpoint

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,11 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
 
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
