f5f532d74824d400d0539e6267f9cbe70cc09327
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-21 21:55:59 -0500

    refactor: created root endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
+
+
