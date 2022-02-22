6925a04b394693533937f8078d2f623a2eb4f7dd
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-21 22:02:20 -0500

    refactor: registered manual hello to a route endpoint

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,15 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
+
+async fn main() -> std::io::Result<()> {
+    HttpServer:new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")
+    .run()
+    .await
+}
